27fe286ad14f03fee4030be634707ddbdc3b8900
Ben Haugen <ben1232jazz@hotmail.com>
2018-09-18 14:46:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var katzDeli = [];
+const takeANumber = (katzDeli) => { 
+  var number = 0
+  katzDeli.push(number)
+  while (number < katzDelil.length)
+  number++;
+  
+
+  return (`Welcome, you are number ${number}`);
+}
